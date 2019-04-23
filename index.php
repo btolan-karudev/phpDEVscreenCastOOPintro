148b33e9ace2de754a3508198cbb139ecd7170df
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-04-23 19:42:03 +0200

    encapsulation

--- index.php
+++ index.php
@@ -1,40 +1,61 @@
 <?php
 
-class Video{
+class Video
+{
 
-    public $type;
-    public $duration;
-    public $published = false;
-    public $title;
+    private $type;
+    private $duration;
+    protected $published = false;
+    private $title;
+    private $playStatus;
 
-    public function __construct(string $type, float $duration, string $title)
-    {
-        $this->type = $type;
-        $this->duration = $duration;
-        $this->title = $title;
-    }
 
     public function play()
     {
-        return $this->published ? "The video is playing" : "This video is not yet available";
-    }
-    
-    public function pause()
-    {
-        return $this->published ? 'The video is paused':"";
+        if ($this->getPublished()) {
+            $this->playStatus = true;
+            return "The video is playing";
+        }
+        return "This video is not yet available";
     }
 
-    public function __destruct()
+    public function pause()
     {
-        var_dump('Destroing instance of '. get_class());
+        return $this->playStatus ? 'The video is paused' : "";
+    }
+
+
+
+    public function setPublished(bool $state)
+    {
+        $this->published = $state;
+    }
+
+    private function getPublished() {
+        return $this->published;
+    }
+
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle() {
+        return $this->title;
     }
 
 }
+header('Content-Type:text/plain', true);
+$introduction = new Video;
+$introduction->setPublished(true);
 
-//header('Content-Type:text/plain', true);
-$introduction = new Video('mp4', 10.5, 'Intro to OOP');
+$introduction->setTitle('Intro to OOP');
+echo $introduction->getTitle(), PHP_EOL;
+echo $introduction->play(), PHP_EOL, $introduction->pause(), PHP_EOL;
 
-$video2 = new Video('mp4', 6525.354, 'video2');
-var_dump($video2);
-exit;
+
+
+
+
+
 
